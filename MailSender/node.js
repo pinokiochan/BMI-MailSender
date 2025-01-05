9b84b96710414366f367e372fdcf80c2649bbddc
pinokiochan <beibarys7ergaliev@gmail.com>
2025-01-05 20:54:08 +0500

    edited

--- MailSender/node.js
+++ MailSender/node.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+    require('dotenv').config();
 
-const nodemailer = require('nodemailer');
+    const nodemailer = require('nodemailer');
 
-const message = {
-    from: process.env.EMAIL_USER,
-    to: "tamutdzhin@gmail.com",
-    subject: "BHA 0 : 2 ARSENAL",
-    text: "PRIVET",
-};
+    const message = {
+        from: process.env.EMAIL_USER,
+        to: "sholak012@gmail.com",
+        subject: "Salem sulu karyndas",
+        text: "tyndap tyrmyn aita ber",
+    };
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.mail.me.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+    const transporter = nodemailer.createTransport({
+        host: "smtp.mail.me.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-transporter.sendMail(message);
+    transporter.sendMail(message);
